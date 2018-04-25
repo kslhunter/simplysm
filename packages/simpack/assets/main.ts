2c6d8f02113f220c527d9fba7ed686e0410a857d
kslhunter <kslhunter@gmail.com>
2018-04-26 03:34:15 +0900

    v6.0.44

--- packages/simpack/assets/main.ts
+++ packages/simpack/assets/main.ts
@@ -1,10 +1,9 @@
 import {enableProdMode} from "@angular/core";
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import "zone.js/dist/zone";
-import {AppModule} from "./AppModule";
 
 if (process.env.NODE_ENV === "production") {
     enableProdMode();
 }
 
-(async () => await platformBrowserDynamic().bootstrapModule(AppModule))();
+(async () => await platformBrowserDynamic().bootstrapModule(require("APP_MODULE_PATH").AppModule))();
