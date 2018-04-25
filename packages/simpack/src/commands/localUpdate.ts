840b2a62872941a01e18ab711edce42397d47fb9
kslhunter <kslhunter@gmail.com>
2018-04-26 02:22:28 +0900

    v6.0.41

---
+++ packages/simpack/src/commands/localUpdate.ts
@@ -0,0 +1,28 @@
+import * as fs from "fs-extra";
+import * as path from "path";
+import {LibraryBuilder} from "../builders/LibraryBuilder";
+import {LocalUpdater} from "../builders/LocalUpdater";
+
+export async function build(argv: { watch: boolean }): Promise<void> {
+    if (
+        argv.watch &&
+        path.basename(process.cwd()) !== "simplism" &&
+        fs.existsSync(path.resolve(process.cwd(), "../simplism"))
+    ) {
+        const rootPackageJson = fs.readJsonSync(path.resolve(process.cwd(), "package.json"));
+        const dependencySimplismPackageNameList = [
+            ...rootPackageJson.dependencies ? Object.keys(rootPackageJson.dependencies) : [],
+            ...rootPackageJson.devDependencies ? Object.keys(rootPackageJson.devDependencies) : []
+        ].filter((item) => item.startsWith("@simplism")).map((item) => item.slice(10));
+
+        for (const dependencySimplismPackageName of dependencySimplismPackageNameList) {
+            await new LocalUpdater(dependencySimplismPackageName).runAsync(true);
+        }
+    }
+
+    const promiseList: Promise<void>[] = [];
+    for (const packageName of fs.readdirSync(path.resolve(process.cwd(), `packages`))) {
+        promiseList.push(new LibraryBuilder(packageName).runAsync(argv.watch));
+    }
+    await Promise.all(promiseList);
+}
