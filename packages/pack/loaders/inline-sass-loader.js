263e6f64adf8af457d69414274055fc97de098e4
kslhunter <kslhunter@gmail.com>
2018-05-21 16:54:09 +0900

    최초 커밋

---
+++ packages/pack/loaders/inline-sass-loader.js
@@ -0,0 +1,41 @@
+const loaderUtils = require("loader-utils");
+const sass = require("node-sass");
+const path = require("path");
+
+module.exports = function (content, sourceMap) {
+  if (this.cacheable) {
+    this.cacheable();
+  }
+
+  const options = loaderUtils.getOptions(this) || {};
+
+  const scssRegex = /\/\* *language=SCSS *\*\/ *[`"'](((?![^\\]['"`])(.|\r|\n))*)[^\\]['"`]/;
+
+  let newContent = content;
+  const matches = content.match(new RegExp(scssRegex, "gi"));
+  if (matches) {
+    const results = matches.map(match => {
+      try {
+        return sass.renderSync({
+          file: this.resourcePath,
+          data: JSON.parse(`"${match.match(scssRegex)[1]}"`),
+          sourceMapEmbed: options.sourceMap
+        });
+      }
+      catch (err) {
+        console.error(match);
+        throw err;
+      }
+    });
+
+    const includedFiles = results.map(result => result.stats.includedFiles).reduce((a, b) => a.concat(b));
+    for (const includedFile of includedFiles) {
+      this.dependency(path.normalize(includedFile));
+    }
+
+    let i = 0;
+    newContent = newContent.replace(new RegExp(scssRegex, "gi"), () => "`" + results[i++].css + "`");
+  }
+
+  this.callback(undefined, newContent, sourceMap);
+};
