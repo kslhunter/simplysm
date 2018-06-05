cf20d44f6be40efa503964a4587f31a3a2935c5c
kslhunter <kslhunter@gmail.com>
2018-06-06 04:40:27 +0900

    .

--- packages/angular-hmr/src/HmrAppModuleBase.ts
+++ packages/angular-hmr/src/HmrAppModuleBase.ts
@@ -2,22 +2,12 @@ import {ApplicationRef} from "@angular/core";
 import {createNewHosts, removeNgStyles} from "@angularclass/hmr";
 
 export class HmrAppModuleBase {
-  public constructor(protected readonly _appRef: ApplicationRef/*,
-                     protected readonly _hmrStore: HmrStoreProvider*/) {
+  public constructor(protected readonly _appRef: ApplicationRef) {
   }
-
-  /*public hmrOnInit(store: any): any {
-    if (!store || !store.state) return;
-    this._hmrStore.state = store.state;
-
-    this._appRef.tick();
-    delete store.state;
-  }*/
 
   public hmrOnDestroy(store: any): any {
     const cmpLocation = this._appRef.components.map(cmp => cmp.location.nativeElement);
     store.disposeOldHosts = createNewHosts(cmpLocation);
-    /*store.state = this._hmrStore.state;*/
     removeNgStyles();
   }
 
