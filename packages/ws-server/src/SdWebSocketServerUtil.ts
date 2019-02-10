64e16f6bb66612996c2f3cffeec11d42a8b293d1
kslhunter <kslhunter@gmail.com>
2019-02-10 17:46:07 +0900

    .

--- packages/ws-server/src/SdWebSocketServerUtil.ts
+++ packages/ws-server/src/SdWebSocketServerUtil.ts
@@ -1,16 +1,38 @@
 import * as url from "url";
 import * as fs from "fs-extra";
 import * as path from "path";
+import * as glob from "glob";
 
 export class SdWebSocketServerUtil {
   public static async getConfigAsync(staticPath: string, requestUrl: string): Promise<any> {
     const urlObj = url.parse(requestUrl!, true, false);
     const urlPath = decodeURI(urlObj.pathname!);
 
-    if (urlPath.startsWith("/")) {
+
+    if (urlPath !== "/") {
       return await fs.readJson(path.resolve(staticPath, "." + urlPath, "configs.json"));
     }
+    else {
+      await new Promise<void>((resolve, reject) => {
+        glob(path.resolve(staticPath, "*", "*", "configs.json"), async (err, files) => {
+          if (err) {
+            reject(err);
+            return;
+          }
 
-    throw new Error("미구현");
+          for (const file of files) {
+            const config = await fs.readJson(file);
+            if (config.vhost && config.vhost === urlObj.hostname) {
+              resolve(config);
+              return;
+            }
+          }
+
+          reject(new Error("서버에서 이 패키지의 설정파일을 찾는데 실패하였습니다."));
+        });
+      });
+
+      throw new Error("미구현");
+    }
   }
 }
