5e29b6890ddc080683e0f34d02da0bdbf58bf81d
kslhunter <kslhunter@gmail.com>
2019-02-23 14:09:59 +0900

    fileDialog에서 파일을 선택하지 않고 닫았을때 반환하지 않는 현상 수정

--- packages/angular/src/providers/SdFileDialogProvider.ts
+++ packages/angular/src/providers/SdFileDialogProvider.ts
@@ -1,3 +1,5 @@
+import {Wait} from "@simplysm/common";
+
 export class SdFileDialogProvider {
   public async showAsync(): Promise<File | undefined> {
     return await new Promise<File | undefined>(resolve => {
@@ -5,10 +7,26 @@ export class SdFileDialogProvider {
       inputEl.type = "file";
       inputEl.multiple = false;
       inputEl.onchange = (event: Event) => {
+        document.body.removeChild(inputEl);
+
         const file = event.target!["files"][0];
         resolve(file);
       };
+      inputEl.style.opacity = "0";
+      inputEl.style.position = "fixed";
+      inputEl.style.top = "0";
+      inputEl.style.left = "0";
+      inputEl.style.pointerEvents = "none";
+      document.body.appendChild(inputEl);
+      inputEl.focus();
       inputEl.click();
+
+      inputEl.onfocus = async () => {
+        document.body.removeChild(inputEl);
+
+        await Wait.time(100);
+        resolve();
+      };
     });
   }
 }
