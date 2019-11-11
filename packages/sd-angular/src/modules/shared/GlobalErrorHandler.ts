5597bcef8e9ceb0320cefef6de20c4fbdf875b0e
kslhunter <kslhunter@gmail.com>
2019-11-12 08:29:22 +0900

    sd-angular > 에러 발생시 화면 멈춤 현상 수정

--- packages/sd-angular/src/modules/shared/GlobalErrorHandler.ts
+++ packages/sd-angular/src/modules/shared/GlobalErrorHandler.ts
@@ -10,27 +10,27 @@ export class GlobalErrorHandler implements ErrorHandler {
                      private readonly _ngModule: NgModuleRef<any>) {
   }
 
-  public async handleError(error: any): Promise<void> {
+  public handleError(error: any): void {
     if (this._isProcessing) {
       return;
     }
     this._isProcessing = true;
 
     const err: Error = error.rejection ? error.rejection : error;
-    console.error(err);
 
     const appRef = this._ngModule.injector.get(ApplicationRef);
 
     // 새 엘리먼트 넣기
     const prevEls = appRef.components.map(cmp => cmp.location.nativeElement).filterExists();
 
-    if (process.env.NODE_ENV === "production") {
-      const isUserSendFnSuccess = await this._log.write({stack: err.stack, type: "error"});
+    setTimeout(async () => {
+      if (process.env.NODE_ENV === "production") {
+        const isUserSendFnSuccess = await this._log.write({stack: err.stack, type: "error"});
 
-      for (const prevEl of prevEls) {
-        const newEl = document.createElement(prevEl.tagName);
-        if (process.env.NODE_ENV === "production") {
-          (newEl as HTMLElement).innerHTML = `
+        for (const prevEl of prevEls) {
+          const newEl = document.createElement(prevEl.tagName);
+          if (process.env.NODE_ENV === "production") {
+            (newEl as HTMLElement).innerHTML = `
 처리되지않은 오류가 발생하였습니다.<br/>
 자세한 사항은 아래 정보와 함께 관리자에게 문의하세요.<br/>
 --<br/>
@@ -38,34 +38,37 @@ export class GlobalErrorHandler implements ErrorHandler {
 ${new DateTime().toFormatString("yyyy-MM-dd HH:mm:ss.fff")}<br/>
 오류메시지:<br/>
 <pre>${isUserSendFnSuccess ? err.message : err.stack}</pre>`;
+          }
+
+          prevEl.parentNode.insertBefore(newEl, prevEl);
         }
 
-        prevEl.parentNode.insertBefore(newEl, prevEl);
+        try {
+          this._ngModule.destroy();
+        }
+        catch {
+        }
       }
+      else {
+        const divEl = document.createElement("div");
+        divEl.style.position = "fixed";
+        divEl.style.top = "0";
+        divEl.style.left = "0";
+        divEl.style.width = "100%";
+        divEl.style.height = "100%";
+        divEl.style.color = "white";
+        divEl.style.background = "rgba(0,0,0,.6)";
+        divEl.style.zIndex = "9999";
+        divEl.style.overflow = "auto";
+        divEl.style.padding = "4px";
 
-      try {
-        this._ngModule.destroy();
+        divEl.innerHTML = `<pre style="font-size: 12px; font-family: 'D2Coding', monospace; line-height: 1.2em;">${err.stack}</pre>`;
+        prevEls[0].appendChild(divEl);
+
+        await this._log.write({stack: err.stack, type: "error"});
       }
-      catch {
-      }
-    }
-    else {
-      const divEl = document.createElement("div");
-      divEl.style.position = "fixed";
-      divEl.style.top = "0";
-      divEl.style.left = "0";
-      divEl.style.width = "100%";
-      divEl.style.height = "100%";
-      divEl.style.color = "white";
-      divEl.style.background = "rgba(0,0,0,.6)";
-      divEl.style.zIndex = "9999";
-      divEl.style.overflow = "auto";
-      divEl.style.padding = "4px";
+    });
 
-      divEl.innerHTML = `<pre style="font-size: 12px; font-family: 'D2Coding', monospace; line-height: 1.2em;">${err.stack}</pre>`;
-      prevEls[0].appendChild(divEl);
-
-      await this._log.write({stack: err.stack, type: "error"});
-    }
+    throw err;
   }
 }
