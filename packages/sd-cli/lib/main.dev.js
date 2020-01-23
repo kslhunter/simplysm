fd4b41f5e340ad158a8450d2aa6e2dcfb9e58f0c
kslhunter <kslhunter@gmail.com>
2020-01-23 23:41:18 +0900

    3.x 버전 최초 구성중

--- packages/sd-cli/lib/main.dev.js
+++ packages/sd-cli/lib/main.dev.js
@@ -1,4 +1,5 @@
 require("@simplysm/sd-core-common");
+require("element-qsa-scope");
 require("core-js/proposals/reflect-metadata");
 require("zone.js/dist/zone");
 
@@ -9,59 +10,51 @@ const AppModuleNgFactory = require("SD_APP_MODULE_FACTORY").AppModuleNgFactory;
 let ngModuleRef;
 
 function start() {
-  if (window.navigator.userAgent.includes("MSIE ")) {
-    platformBrowserDynamic().bootstrapModuleFactory(AppModuleNgFactory)
-      .catch(err => {
-        console.error(err);
-      });
-  }
-  else {
-    module["hot"].accept();
+  module["hot"].accept();
 
-    platformBrowserDynamic().bootstrapModuleFactory(AppModuleNgFactory)
-      .then(mod => {
-        ngModuleRef = mod;
-      })
-      .catch(err => {
-        console.error(err);
-      });
+  platformBrowserDynamic().bootstrapModuleFactory(AppModuleNgFactory)
+    .then(mod => {
+      ngModuleRef = mod;
+    })
+    .catch(err => {
+      console.error(err);
+    });
 
-    module["hot"].dispose(() => {
-      // console.clear();
-      const appRef = ngModuleRef.injector.get(ApplicationRef);
+  module["hot"].dispose(() => {
+    // console.clear();
+    const appRef = ngModuleRef.injector.get(ApplicationRef);
 
-      // 새 엘리먼트 넣기
-      const prevEls = appRef.components.map(cmp => cmp.location.nativeElement);
-      for (const prevEl of prevEls) {
-        const newEl = document.createElement(prevEl.tagName);
-        prevEl.parentNode.insertBefore(newEl, prevEl);
-      }
+    // 새 엘리먼트 넣기
+    const prevEls = appRef.components.map(cmp => cmp.location.nativeElement);
+    for (const prevEl of prevEls) {
+      const newEl = document.createElement(prevEl.tagName);
+      prevEl.parentNode.insertBefore(newEl, prevEl);
+    }
 
-      // 이전 스타일 삭제
-      const prevNgStyleEls = Array.from(document.head.querySelectorAll(":scope > style"))
-        .filter(styleEl => styleEl.innerText.indexOf("_ng") !== -1);
+    // 이전 스타일 삭제
+    const prevNgStyleEls = Array.from(document.head.querySelectorAll(":scope > style"))
+      .filter(styleEl => styleEl.innerText.indexOf("_ng") !== -1);
 
-      for (const prevNgStyleEl of prevNgStyleEls) {
-        document.head.removeChild(prevNgStyleEl);
-      }
+    for (const prevNgStyleEl of prevNgStyleEls) {
+      document.head.removeChild(prevNgStyleEl);
+    }
 
-      // 이전 ngModule 지우기
+    // 이전 ngModule 지우기
+    try {
+      ngModuleRef.destroy();
+    }
+    catch (err) {
+    }
+
+    // 이전 엘리먼트 삭제
+    for (const prevEl of prevEls) {
       try {
-        ngModuleRef.destroy();
+        prevEl.parentNode.removeChild(prevEl);
       }
       catch (err) {
       }
-
-      // 이전 엘리먼트 삭제
-      for (const prevEl of prevEls) {
-        try {
-          prevEl.parentNode.removeChild(prevEl);
-        }
-        catch (err) {
-        }
-      }
-    });
-  }
+    }
+  });
 }
 
 start();
