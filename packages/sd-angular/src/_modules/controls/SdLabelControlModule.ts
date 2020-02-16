4a6c47ad5bf1008df4d747bc4692a7ba9eaed457
김석래 <kslhunter@gmail.com>
2020-02-17 08:35:47 +0900

    3.x 버전 최초 구성중

---
+++ packages/sd-angular/src/_modules/controls/SdLabelControlModule.ts
@@ -0,0 +1,15 @@
+import {NgModule} from "@angular/core";
+import {SdLabelControl} from "../../controls/SdLabelControl";
+
+@NgModule({
+  imports: [],
+  declarations: [
+    SdLabelControl
+  ],
+  exports: [
+    SdLabelControl
+  ],
+  entryComponents: []
+})
+export class SdLabelControlModule {
+}
---
+++ packages/sd-angular/src/_modules/controls/SdMarkdownEditorControlModule.ts
@@ -0,0 +1,15 @@
+import {NgModule} from "@angular/core";
+import {SdMarkdownEditorControl} from "../../controls/SdMarkdownEditorControl";
+
+@NgModule({
+  imports: [],
+  declarations: [
+    SdMarkdownEditorControl
+  ],
+  exports: [
+    SdMarkdownEditorControl
+  ],
+  entryComponents: []
+})
+export class SdMarkdownEditorControlModule {
+}
