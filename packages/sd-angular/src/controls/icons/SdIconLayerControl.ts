a860c5f1a57814412b252d99f507bddf7cb9fddc
김석래 <kslhunter@gmail.com>
2020-04-12 22:43:50 +0900

    3.x 버전 최초 구성중

--- packages/sd-angular/src/controls/icons/SdIconLayerControl.ts
+++ packages/sd-angular/src/controls/icons/SdIconLayerControl.ts
@@ -19,7 +19,7 @@ import {SdInputValidate} from "../../commons/SdInputValidate";
   selector: "sd-icon-layer",
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <ng-content select="fa-icon, fa-layers-text, fa-layers-counter"></ng-content>`,
+    <ng-content></ng-content>`,
   styles: [/* language=SCSS */ `
     sd-icon-layer {
       &[sd-fixed-width=true] {
