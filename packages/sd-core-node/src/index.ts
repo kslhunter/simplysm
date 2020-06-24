529dc93184473e33022aa4ac149d1cbd529c473f
kslhunter <kslhunter@gmail.com>
2020-06-24 16:26:52 +0900

    v3.4.80

--- packages/sd-core-node/src/index.ts
+++ packages/sd-core-node/src/index.ts
@@ -2,7 +2,7 @@ import "@simplysm/sd-core-common";
 export * from "./FsUtils";
 export * from "./FsWatcher";
 export * from "./Logger";
+export * from "./NetworkUtils";
 export * from "./ProcessManager";
 export * from "./ProcessWorkManager";
 export * from "./SdProcessWorker";
-export * from "./NetworkUtils";
