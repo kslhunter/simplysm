4938efbc207a5e4394bac753cef1326eb7046d26
kslhunter <kslhunter@gmail.com>
2021-10-08 21:27:49 +0900

    .

--- packages/eslint-plugin/src/configs/angular.js
+++ packages/eslint-plugin/src/configs/angular.js
@@ -28,9 +28,6 @@ module.exports = {
     "@angular-eslint/no-output-rename": "off",
     "@angular-eslint/contextual-decorator": "off",
     "@angular-eslint/sort-ngmodule-metadata-arrays": "off",
-    "@angular-eslint/no-empty-lifecycle-method": "warn",
-
-    // ---아래 내용들 체크필요?
-    "@angular-eslint/use-injectable-provided-in": "off"
+    "@angular-eslint/no-empty-lifecycle-method": "warn"
   }
 };
