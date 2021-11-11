cda000aed9536de45a64eff7480fee757a87406a
kslhunter <kslhunter@gmail.com>
2021-11-11 16:37:05 +0900

    SdCliPrepare 수정

--- packages/sd-cli/src/entry-points/SdCliPrepare.ts
+++ packages/sd-cli/src/entry-points/SdCliPrepare.ts
@@ -1,18 +1,27 @@
-import { FsUtil } from "@simplysm/sd-core-node";
+import { FsUtil, Logger } from "@simplysm/sd-core-node";
 
 export class SdCliPrepare {
+  private readonly _logger = Logger.get(["simplysm", "sd-cli", this.constructor.name]);
+
   public async prepareAsync(): Promise<void> {
-    // 타입체크 속도 지연 메시지
-    await this._modifyTypescriptCodeForTypeCheckPerformanceWarning();
+    // 타입체크 속도 지연 메시지 추가
+    const r1 = await this._modifyTypescriptCodeForTypeCheckPerformanceWarning();
+    this._logger.log(`[모듈수정] 1. 타입체크 속도 지연 메시지 추가 (${r1 ? "O" : "X"})`);
+
+    // ngcc 동시작업 관련: ng-packagr 무조건 Async
+    const r2 = await this._modifyNgccProcessorNgPackagrCodeForAsync();
+    this._logger.log(`[모듈수정] 2. ngcc: 비동기 처리 (${r2 ? "O" : "X"})`);
 
     // ngcc 동시작업 관련: 대기시간 늘림
-    await this._modifyNgccProcessorConfigurationCodeForLongWaitTime();
+    const r3 = await this._modifyNgccProcessorConfigurationCodeForLongWaitTime();
+    this._logger.log(`[모듈수정] 3. ngcc: 대기시간 늘림 (${r3 ? "O" : "X"})`);
 
     // ngcc 동시작업 관련: 디버깅 메시지 끄기
-    await this._modifyNgccProcessorAsyncLockerCodeForRemoveWaitingMessage();
+    const r4 = await this._modifyNgccProcessorAsyncLockerCodeForRemoveWaitingMessage();
+    this._logger.log(`[모듈수정] 3. ngcc: 불필요 메시지 제거 (${r4 ? "O" : "X"})`);
   }
 
-  private async _modifyTypescriptCodeForTypeCheckPerformanceWarning(): Promise<void> {
+  private async _modifyTypescriptCodeForTypeCheckPerformanceWarning(): Promise<boolean> {
     const filePath = require.resolve("typescript");
     const fileContent = await FsUtil.readFileAsync(filePath);
     const modifiedFileContent = fileContent.replace(`
@@ -51,24 +60,37 @@ export class SdCliPrepare {
             }
         }`);
     await FsUtil.writeFileAsync(filePath, modifiedFileContent);
+    return fileContent !== modifiedFileContent;
   }
 
-  private async _modifyNgccProcessorConfigurationCodeForLongWaitTime(): Promise<void> {
+  private async _modifyNgccProcessorNgPackagrCodeForAsync(): Promise<boolean> {
+    const filePath = require.resolve("ng-packagr/lib/ngc/ngcc-processor.js");
+    const fileContent = await FsUtil.readFileAsync(filePath);
+    const modifiedFileContent = fileContent.replace(`
+        ngcc_1.process({
+            basePath: this._nodeModulesDirectory`, `
+        ngcc_1.process({
+            async: true,
+            basePath: this._nodeModulesDirectory`); // 10분
+    await FsUtil.writeFileAsync(filePath, modifiedFileContent);
+    return fileContent !== modifiedFileContent;
+  }
+
+  private async _modifyNgccProcessorConfigurationCodeForLongWaitTime(): Promise<boolean> {
     const filePath = require.resolve("@angular/compiler-cli/ngcc/src/packages/configuration.js");
     const fileContent = await FsUtil.readFileAsync(filePath);
     const modifiedFileContent = fileContent.replace(`
         locking: {
             retryDelay: 500,
-            retryAttempts: 500,
-        }`, `
+            retryAttempts: 500`, `
         locking: {
             retryDelay: 1000,
-            retryAttempts: 10 * 60,
-        }`); // 10분
+            retryAttempts: 10 * 60`); // 10분
     await FsUtil.writeFileAsync(filePath, modifiedFileContent);
+    return fileContent !== modifiedFileContent;
   }
 
-  private async _modifyNgccProcessorAsyncLockerCodeForRemoveWaitingMessage(): Promise<void> {
+  private async _modifyNgccProcessorAsyncLockerCodeForRemoveWaitingMessage(): Promise<boolean> {
     const filePath = require.resolve("@angular/compiler-cli/ngcc/src/locking/async_locker.js");
     const fileContent = await FsUtil.readFileAsync(filePath);
     const modifiedFileContent = fileContent.replace(`
@@ -76,5 +98,6 @@ export class SdCliPrepare {
                                     ("Waiting up to " + this.retryDelay * this.retryAttempts / 1000 + "s for it to finish.\\n") +
                                     ("(If you are sure no ngcc process is running then you should delete the lock-file at " + this.lockFile.path + ".)"));`, ``);
     await FsUtil.writeFileAsync(filePath, modifiedFileContent);
+    return fileContent !== modifiedFileContent;
   }
 }
