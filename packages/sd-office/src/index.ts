a5e626432128c8b57d1f4c184a8c126234e76de6
kslhunter <kslhunter@gmail.com>
2021-11-15 18:44:01 +0900

    #218

--- packages/sd-office/src/index.ts
+++ packages/sd-office/src/index.ts
@@ -1,4 +1,7 @@
 import "@simplysm/sd-core-common";
+export * from "./reader/SdExcelReader";
+export * from "./reader/SdExcelReaderDataTable";
+export * from "./reader/SdExcelReaderWorkSheet";
 export * from "./SdExcelCell";
 export * from "./SdExcelCellStyle";
 export * from "./SdExcelColumn";
---
+++ packages/sd-office/src/reader/SdExcelReader.ts
@@ -0,0 +1,27 @@
+import * as XLSX from "xlsx";
+import { SdExcelReaderWorkSheet } from "./SdExcelReaderWorkSheet";
+
+export class SdExcelReader {
+  private readonly _wb: XLSX.WorkBook;
+
+  public constructor(data: Uint8Array) {
+    this._wb = XLSX.read(data, { type: "array", codepage: 949 });
+  }
+
+  public getWorkSheet(name: string): SdExcelReaderWorkSheet
+  public getWorkSheet(index: number): SdExcelReaderWorkSheet
+  public getWorkSheet(arg: string | number): SdExcelReaderWorkSheet {
+    if (typeof arg === "number") {
+      if (!this._wb.SheetNames[arg]) {
+        throw new Error(`${arg + 1}번째 시트를 찾을 수 없습니다.`);
+      }
+      return new SdExcelReaderWorkSheet(this._wb.Sheets[this._wb.SheetNames[0]]);
+    }
+    else {
+      if (!this._wb.SheetNames.includes(arg)) {
+        throw new Error(`시트'${arg}'를 찾을 수 없습니다.`);
+      }
+      return new SdExcelReaderWorkSheet(this._wb.Sheets[arg]);
+    }
+  }
+}
