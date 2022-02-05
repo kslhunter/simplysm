744e782698e0ae06258c1d47b7cb8f7c593d3fb6
kslhunter <kslhunter@gmail.com>
2022-02-05 16:10:05 +0900

    .

--- packages/sd-core-node/src/utils/SdProcess.ts
+++ packages/sd-core-node/src/utils/SdProcess.ts
@@ -1,9 +1,13 @@
-import child_process from "child_process";
+import cp from "child_process";
 
 export class SdProcess {
-  public static async spawnAsync(cmd: string, opts?: child_process.ExecOptions): Promise<string> {
+  public static async spawnAsync(cmd: string, opts?: cp.ExecOptions): Promise<string> {
     return await new Promise<string>((resolve, reject) => {
-      const ps = child_process.spawn(cmd.split(" ")[0], cmd.split(" ").slice(1), opts);
+      const ps = cp.spawn(cmd.split(" ")[0], cmd.split(" ").slice(1), {
+        shell: true,
+        stdio: "pipe",
+        ...opts
+      });
       ps.on("error", (err) => {
         reject(err);
       });
