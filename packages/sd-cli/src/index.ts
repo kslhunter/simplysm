2c0d517fed5809491ff34ee21f54a97c7bda7579
kslhunter <kslhunter@gmail.com>
2022-02-21 16:58:11 +0900

    v7.0.66

--- packages/sd-cli/src/index.ts
+++ packages/sd-cli/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./build-tool/SdCliCacheCompilerHost";
+export * from "./build-tool/SdCliCordova";
 export * from "./build-tool/SdCliIndexFileGenerator";
 export * from "./build-tool/SdCliNgCacheCompilerHost";
 export * from "./build-tool/SdCliPackageLinter";
