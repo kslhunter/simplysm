a63c959413f4a5a7b82e77b10bb51bd997646f7b
김석래 <slkim@simplysm.net>
2023-06-14 14:13:10 +0900

    .

--- packages/sd-angular/src/controls/SdCardControl.ts
+++ packages/sd-angular/src/controls/SdCardControl.ts
@@ -16,35 +16,39 @@ import { sdThemes, TSdTheme } from "../commons";
       background: white;
       border-radius: var(--border-radius-default);
       overflow: hidden;
-      border-top: 3px solid var(--theme-color-primary-default);
+      @include elevation(4);
+      /*border-top: 3px solid var(--theme-color-primary-default);
       border-bottom: 1px solid var(--theme-color-primary-default);
       border-left: 0 solid var(--theme-color-grey-light);
-      border-right: 0 solid var(--theme-color-grey-light);
+      border-right: 0 solid var(--theme-color-grey-light);*/
 
       &[sd-elevation=none] {
-        border-top-width: 0;
-        border-bottom-width: 0;
+        @include elevation(0);
+        //border-top-width: 0;
+        //border-bottom-width: 0;
       }
 
       &[sd-elevation=lg] {
-        border-top-width: 5px;
-        border-left-width: 1px;
-        border-right-width: 1px;
+        @include elevation(8);
+        //border-top-width: 5px;
+        //border-left-width: 1px;
+        //border-right-width: 1px;
         border-radius: var(--border-radius-xl);
       }
 
       &[sd-elevation=xl] {
-        border-top-width: 7px;
-        border-left-width: 1px;
-        border-right-width: 1px;
+        @include elevation(16);
+        //border-top-width: 7px;
+        //border-left-width: 1px;
+        //border-right-width: 1px;
         border-radius: var(--border-radius-xxl);
       }
 
-      @each $color in $arr-theme-color {
-        &[sd-theme=#{$color}] {
-          border-color: var(--theme-color-#{$color}-default);
-        }
-      }
+      //@each $color in $arr-theme-color {
+      //  &[sd-theme=#{$color}] {
+      //    border-color: var(--theme-color-#{$color}-default);
+      //  }
+      //}
     }
   `]
 })
