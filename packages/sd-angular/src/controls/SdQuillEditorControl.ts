699354b06fe11354dbe974cd9634bb4777ed5f66
kslhunter <kslhunter@gmail.com>
2024-08-14 19:05:03 +0900

    #834

--- packages/sd-angular/src/controls/SdQuillEditorControl.ts
+++ packages/sd-angular/src/controls/SdQuillEditorControl.ts
@@ -27,8 +27,61 @@ Quill.register('modules/resize', QuillResizeImage);
   template: `
     <div></div>`,
   styles: [/* language=SCSS */ `
+    sd-quill-editor {
+      display: block;
+      border: 1px solid var(--theme-grey-lighter);
+      border-radius: var(--border-radius-default);
 
-  `]
+      > .ql-toolbar {
+        border: none !important;
+        border-bottom: 1px solid var(--theme-grey-lighter) !important;
+        padding: var(--gap-sm) var(--gap-default) !important;
+
+        button {
+          font-size: var(--font-size-default);
+          height: 1.75em !important;
+          width: 2em !important;
+          padding: var(--gap-xs) var(--gap-sm) !important;
+          
+          &:hover {
+            color: var(--theme-primary-default) !important;;
+          }
+
+          &.ql-active {
+            color: var(--theme-primary-default) !important;;
+          }
+        }
+      }
+
+      > .ql-container {
+        border: none !important;
+        background: var(--theme-secondary-lightest) !important;
+        border-bottom-left-radius: var(--border-radius-default);
+        border-bottom-right-radius: var(--border-radius-default);
+
+        > .ql-editor {
+          padding: var(--gap-lg) !important;
+          min-height: 4em;
+        }
+      }
+
+      &[sd-disabled1=true] {
+        border: none;
+
+        > .ql-toolbar {
+          display: none !important;
+        }
+
+        > .ql-container {
+          border-top-left-radius: var(--border-radius-default);
+          border-top-right-radius: var(--border-radius-default);
+        }
+      }
+    }
+  `],
+  host: {
+    "[attr.sd-disabled]": "disabled"
+  }
 })
 export class SdQuillEditorControl implements OnInit, DoCheck {
   #elRef = inject<ElementRef<HTMLElement>>(ElementRef);
@@ -59,8 +112,6 @@ export class SdQuillEditorControl implements OnInit, DoCheck {
         disabled: [this.disabled]
       }, () => {
         this.#quill.enable(!this.disabled);
-        const toolbar = this.#quill.getModule("toolbar");
-        console.log(toolbar);
       });
     });
   }
@@ -69,7 +120,20 @@ export class SdQuillEditorControl implements OnInit, DoCheck {
     this.#quill = new Quill(this.#elRef.nativeElement.firstElementChild as HTMLElement, {
       theme: "snow",
       modules: {
-        toolbar: true,
+        toolbar: [
+          [{'header': 1}, {'header': 2}, 'bold', 'italic', 'underline', 'strike'],
+
+          [{'color': []}, {'background': []}],
+
+          [{'list': 'ordered'}, {'list': 'bullet'}],
+
+          [{'indent': '-1'}, {'indent': '+1'}],
+
+          ['blockquote', 'code-block'],
+          [{'align': ''}, {'align': 'center'}, {'align': 'right'}, {'align': 'justify'}],
+
+          ['clean']
+        ],
         resize: {}
       }
     });
