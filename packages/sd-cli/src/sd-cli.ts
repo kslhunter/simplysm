7d60def715496f3592c06109e90e0761bf8b4646
kslhunter <kslhunter@gmail.com>
2025-05-06 02:44:07 +0900

    .

--- packages/sd-cli/src/sd-cli.ts
+++ packages/sd-cli/src/sd-cli.ts
@@ -11,7 +11,7 @@
 //   stdio: "inherit"
 // });
 
-import { ChildProcess, exec, spawn } from "child_process";
+import { exec, spawn } from "child_process";
 import { fileURLToPath } from "node:url";
 import os from "os";
 import path from "path";
@@ -25,49 +25,44 @@ const affinityMask = "0x" + affinityMaskDecimal.toString(16).toUpperCase();
 // console.log(`Affinity Mask (전체 - 1): ${affinityMask}`);
 
 // CLI 실행
-let child: ChildProcess;
-
 const cliPath = fileURLToPath(import.meta.resolve("./sd-cli-entry"));
 
 if (path.extname(cliPath) === ".ts") {
-  child = spawn(
-    "yarn",
-    ["tsx", cliPath, ...process.argv.slice(2)],
-    {
-      stdio: "inherit",
-      env: { ...process.env },
-    },
-  );
-}
-else {
-  child = spawn(
-    "node",
-    ["--import=specifier-resolution-node/register", cliPath, ...process.argv.slice(2)],
-    { stdio: "inherit" },
-  );
-}
-
-child.on("error", (err) => {
-  console.error(err);
-});
-
-
-child.on("spawn", () => {
-  const pid = child.pid;
-
-  const psCommand = `$p = Get-Process -Id ${pid}; $p.ProcessorAffinity = ${affinityMask}`;
-  const command = `powershell -Command "${psCommand}"`;
-
+  const pid = process.pid;
+  const command = `powershell -Command "$p = Get-Process -Id ${pid}; $p.ProcessorAffinity = ${affinityMask}"`;
   exec(command, (err) => {
     if (err) {
-      console.error("Failed to set affinity:", err.message);
+      console.error("Affinity 설정 실패:", err.message);
     }
     else {
-      console.log(`Affinity set to ${affinityMask} for PID ${pid}`);
+      console.log(`Affinity ${affinityMask} 적용됨 (PID: ${pid})`);
     }
   });
-});
+  await import(cliPath);
+}
+else {
+  const child = spawn(
+    "node",
+    ["--import=specifier-resolution-node/register", cliPath, ...process.argv.slice(2)],
+  );
 
-child.on("exit", (code) => {
-  console.log(`CLI exited with code ${code}`);
-});
+  child.on("spawn", () => {
+    const pid = child.pid;
+
+    const psCommand = `$p = Get-Process -Id ${pid}; $p.ProcessorAffinity = ${affinityMask}`;
+    const command = `powershell -Command "${psCommand}"`;
+
+    exec(command, (err) => {
+      if (err) {
+        console.error("Failed to set affinity:", err.message);
+      }
+      else {
+        console.log(`Affinity set to ${affinityMask} for PID ${pid}`);
+      }
+    });
+  });
+
+  child.on("exit", (code) => {
+    console.log(`CLI exited with code ${code}`);
+  });
+}
