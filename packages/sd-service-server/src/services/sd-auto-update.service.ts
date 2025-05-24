75cd7ee04076775e2608213c92c8afe05bbecb1b
kslhunter <kslhunter@gmail.com>
2025-05-24 22:56:26 +0900

    안드로이드 업데이트 로직 개선을 위한 버전 확인 메서드 수정
    - 레거시 업데이트 방식과 신규 업데이트 방식의 호환성을 고려한 메서드 파라미터 수정
    - APK 버전 확인 시 플랫폼과 APK 여부를 명시적으로 처리하도록 변경
    - 업데이트 로직의 유연성을 높이기 위해 주석 추가

--- packages/sd-service-server/src/services/sd-auto-update.service.ts
+++ packages/sd-service-server/src/services/sd-auto-update.service.ts
@@ -5,6 +5,9 @@ import { FsUtils } from "@simplysm/sd-core-node";
 import { SdServiceServerConfUtils } from "../utils/sd-service-server-conf.utils";
 
 export class SdAutoUpdateService extends SdServiceBase {
+  // zip으로 업데이트하는 legacy에서는 apk가 undefined로 들어옴
+  // apk버전으로 변경한 최근것은 client에서 apk가 무조건 true로 들어옴
+  // 신규버전은 일단 sd-cli에서 zip과 apk모두 생성함
   getLastVersion(
     platform: string,
     apk?: boolean,
