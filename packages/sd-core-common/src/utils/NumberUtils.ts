4df7dce400bfb709533ed288a49c9a0f0266b7db
kslhunter <kslhunter@gmail.com>
2025-12-18 18:30:35 +0900

    제목: NumberUtils 숫자 파싱 정규식 오류 수정함
    - [sd-core-common] parseInt 메서드 정규식에서 불필요한 'E' 문자 제거함

--- packages/sd-core-common/src/utils/NumberUtils.ts
+++ packages/sd-core-common/src/utils/NumberUtils.ts
@@ -1,7 +1,7 @@
 export class NumberUtils {
   static parseInt(text: any, radix: number = 10): number | undefined {
     if (typeof text === "number") return Math.round(text);
-    const txt = text?.replace(/[^0-9.\-E]/g, "")?.trim();
+    const txt = text?.replace(/[^0-9.\-]/g, "")?.trim();
     if (txt === undefined) return undefined;
     const result = Number.parseInt(txt, radix);
     if (Number.isNaN(result)) return undefined;
