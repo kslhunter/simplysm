94130f249480e46b7a7a6847e2fad0244c41d968
slkim <slkim@simplysm.co.kr>
2026-02-25 14:27:51 +0900

    feat(sd-cli): translate build-env utils Phase 1 comments to English

--- packages/sd-cli/src/utils/build-env.ts
+++ packages/sd-cli/src/utils/build-env.ts
@@ -2,7 +2,7 @@ import path from "path";
 import fs from "fs/promises";
 
 /**
- * 루트 package.json에서 version 가져오기
+ * Get version from root package.json
  */
 export async function getVersion(cwd: string): Promise<string> {
   const pkgJsonPath = path.join(cwd, "package.json");
